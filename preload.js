112c1dddac7438dd410a54d6550e192ef1a58d51
MicLeh <42337056+legymhueck@users.noreply.github.com>
2026-04-03 11:47:47 +0200

    Comment out DOMContentLoaded event listener
    Comment out the DOMContentLoaded event listener in preload.js.

--- preload.js
+++ preload.js
@@ -1,6 +1,6 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
-window.addEventListener('DOMContentLoaded', () => {
+//window.addEventListener('DOMContentLoaded', () => {
 /*
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
@@ -11,4 +11,4 @@ window.addEventListener('DOMContentLoaded', () => {
     replaceText(`${type}-version`, process.versions[type])
   }
 */  
-})
+//})
